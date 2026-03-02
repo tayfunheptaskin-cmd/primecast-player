477c0af94ad571ddaf28636da29e342f96721571
tayfunheptaskin-cmd <tayfunheptaskin@gmail.com>
2026-03-02 11:37:17 +0100

    Create Theme.kt file with dark theme colors

---
+++ app/src/main/kotlin/com/example/primecastplayer/ui/theme/Theme.kt
@@ -0,0 +1,14 @@
+package com.example.primecastplayer.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val DarkPrimary = Color(0xFFBB86FC)
+val DarkSecondary = Color(0xFF03DAC6)
+val DarkBackground = Color(0xFF121212)
+val DarkSurface = Color(0xFF121212)
+val DarkError = Color(0xFFCF6679)
+val DarkOnPrimary = Color(0xFFFFFFFF)
+val DarkOnSecondary = Color(0xFF000000)
+val DarkOnBackground = Color(0xFFFFFFFF)
+val DarkOnSurface = Color(0xFFFFFFFF)
+val DarkOnError = Color(0xFF000000)
