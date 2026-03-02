72280caab51291b859931d5c9a6f5587bbdacca6
tayfunheptaskin-cmd <tayfunheptaskin@gmail.com>
2026-03-02 11:33:07 +0100

    Create settings.gradle.kts for Android project settings

---
+++ settings.gradle.kts
@@ -0,0 +1,10 @@
+// Android project settings file for Gradle Kotlin DSL.
+
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "primecast-player"
